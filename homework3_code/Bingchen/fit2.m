79bb7a5e9f530224ad7bf9d04017a1964af7daf5
bingchenL1024 <bil009@ucsd.edu>
2022-10-17 20:08:54 -0700

    homework3 functions

---
+++ homework3_code/Bingchen/fit2.m
@@ -0,0 +1,8 @@
+function [data_fit,x] = fit2(data,num_date,P)
+
+A_fit=[ones(length(num_date),1) sin(2*pi*num_date/P) ...
+    cos(2*pi*num_date/P) sin(4*pi*num_date/P) cos(4*pi*num_date/P)];
+x = inv(A_fit'*A_fit)*A_fit'*data;
+data_fit = A_fit*x;
+
+end 
