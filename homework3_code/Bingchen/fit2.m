c787822325b725eda82ef6b21ed9748e792849ba
bingchenL1024 <bil009@ucsd.edu>
2022-10-17 20:38:51 -0700

    update

--- homework3_code/Bingchen/fit2.m
+++ homework3_code/Bingchen/fit2.m
@@ -4,5 +4,6 @@ A_fit=[ones(length(num_date),1) sin(2*pi*num_date/P) ...
     cos(2*pi*num_date/P) sin(4*pi*num_date/P) cos(4*pi*num_date/P)];
 x = inv(A_fit'*A_fit)*A_fit'*data;
 data_fit = A_fit*x;
+x(6) = norm(x(2:5));
 
 end 
