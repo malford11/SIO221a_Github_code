79365daa533b1cdd7e78f1e0ba074a12abb17cfd
Benjamin Werb <55675635+Benwerb@users.noreply.github.com>
2022-11-15 19:32:46 -0800

    updated

--- class10_coding_github_exercise/Class10_Ben.m
+++ class10_coding_github_exercise/Class10_Ben.m
@@ -1,4 +1,4 @@
-function [data,p] = Class10_Ben(ChunkSize)
+function [data] = Class10_Ben(ChunkSize)
 %Class10 Plots mean spectrum
 load class10_record.mat %load data
 data = class10_record; %make a nice struct
@@ -6,12 +6,12 @@ data.dnum = datevec(data.time); %easy to read dates
 NChunks = floor(length(data.time) / ChunkSize); %calc number of chunks
 data.data = detrend(data.data); %detrend data
 data.Chunks = reshape(data.data,ChunkSize,NChunks); %chunkify
-data.dt = (30/60)/24; %convert dt=30 min into CPD
+data.dt =mean(diff(data.time),'omitnan'); %(30/60)/24; %convert dt=30 min into CPD
 N=size(data.Chunks);
 
 [data.spectra,data.f_vector,data.df] = Spectra2(data.Chunks,data.dt,'none');
 %calculate spectra
-[p] = Parseval(data.data,data.spectra,data.df);
+[data.p,data.variance,data.sum_spec] = Parseval(data.data,data.spectra,data.df);
 %check Parseval's theorem
 fig = figure(1);
 loglog(data.f_vector,abs(data.spectra))
